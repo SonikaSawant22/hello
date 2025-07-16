38cf813ac436015d83c4736dadbb029139be9aa5
Sonika Sawant <sonikasawant2235@gmail.com>
2025-07-16 11:53:50 +0530

    Update hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+# List of numbers
+numbers = [10, 20, 30, 40, 50]
+
+# Calculate the sum of numbers
+total = sum(numbers)
+
+# Calculate the average
+average = total / len(numbers)
+
+# Print the result
+print("Numbers:", numbers)
+print("Average:", average)
